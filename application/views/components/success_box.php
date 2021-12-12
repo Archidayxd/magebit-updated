0a53e2e99ec690aa5ba1c26d46896eab9763b3e4
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-12-12 17:38:47 +0200

    fixed img in succes page

--- application/views/components/success_box.php
+++ application/views/components/success_box.php
@@ -1,5 +1,5 @@
  <div class="media-wrapper">
-     <div class="succes-image sub-wrapper"><img id="succesImage" src="/img/Union.svg" alt="cup image"></div>
+     <div class="succes-image sub-wrapper"><img id="succesImage" src="../../../web/img/union.svg" alt="cup image"></div>
      <div class="subscription__title sub-wrapper title-succes" id="title"><span>Thanks for subscribing!</span></div>
      <div class="subscription__subtitle sub-wrapper" id="subtitle"><span>You have successfully subscribed to our email listing. Check your email for the discount code.</span></div>
      <span id="status"></span>

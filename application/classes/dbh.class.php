3c4ac88c95b354a6d43e66dd3598f0fecab38170
Artjoms Atroscenko <Atroscenko@yahoo.com>
2021-12-13 17:14:18 +0200

    add charset to prevent from sql inqection

--- application/classes/dbh.class.php
+++ application/classes/dbh.class.php
@@ -8,10 +8,11 @@ class Dbh{
 
     public function connect()
     {
-        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+        $dsn = 'mysql:host=' . $this->host .';charset=utf8'. ';dbname=' . $this->dbName;
         try {
             $pdo = new PDO($dsn, $this->user, $this->pass);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             return $pdo;
         } catch (PDOException) {
             echo "Error appeared in connection";
